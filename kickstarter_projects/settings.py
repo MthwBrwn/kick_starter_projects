d5499dda63dc991f93adc4d05bbdd50dbe401451
Hannah Sindorf <hannah@hsindorf.com>
2019-01-15 16:41:57 -0800

    installed django-redis

--- kickstarter_projects/settings.py
+++ kickstarter_projects/settings.py
@@ -137,3 +137,21 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
+
+
+# Caching
+
+# REDIS_URL = 'redis://{}:6379/1'.format(os.getenv('REDIS_HOST'))
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        # 'LOCATION': REDIS_URL,
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        },
+        'KEY_PREFIX': 'example'
+    }
+}
+
+CACHE_TTL = 60 * 1
